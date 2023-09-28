80c35e65e43ba8e15d00051016e52525d9450144
Henrique La Porta <henriqueslaporta@gmail.com>
2023-09-28 14:34:57 +0800

    Initial Spring Initializr

---
+++ backend/src/main/java/com/netdeal/fullstaktask/FullstakTaskApplication.java
@@ -0,0 +1,13 @@
+package com.netdeal.fullstaktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstakTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstakTaskApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/netdeal/fullstaktask/FullstakTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netdeal.fullstaktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstakTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
